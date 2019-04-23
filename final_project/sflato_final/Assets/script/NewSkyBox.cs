1ee097df2f44f91a755df6bb543a02c267e68384
sflato <stefancvitanic@gmail.com>
2019-04-23 17:13:15 -0500

    added new screen shots, also modified trigger script to change skybox

--- final_project/sflato_final/Assets/script/NewSkyBox.cs
+++ final_project/sflato_final/Assets/script/NewSkyBox.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class NewSkyBox : MonoBehaviour
 {
-   
-    public Material skyTwo;
-  
-
+    public bool NextSkyBox = false;
+    //public Material One;
+    //public Material Two;
+    //public Material Three;
+    public Material[] MyMaterials;
+    public int arrayPos;
 
     void OnTriggerEnter(Collider other)
     {
-        RenderSettings.skybox = skyTwo;
+        if (other.gameObject.name == "RightHand")
+        {
+            if (arrayPos < MyMaterials.Length)
+            {
+                arrayPos++;
+            }
+            else { arrayPos = 0; }
+
+            RenderSettings.skybox = MyMaterials[arrayPos];
+            Debug.Log("enter" + other.gameObject.name);
+        }
     }
+    //void OnTriggerExit(Collider other)
+    //{
+    //    NextSkyBox = false;
+    //    //RenderSettings.skybox = One;
+    //    Debug.Log("exit");
+    //}
+
+
+
+   
+
+
 }
